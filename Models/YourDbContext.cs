e77397f9b45b0075a18ab8ff1640666cd2657b78
Tran Hoang Kiet <116096297+sunnyfleur@users.noreply.github.com>
2024-05-17 05:46:18 +0700

    change: assem

--- Models/YourDbContext.cs
+++ Models/YourDbContext.cs
@@ -3,12 +3,12 @@ using Microsoft.Extensions.Configuration;
 using SOA_Assignment.Models;
 using System.IO;
 
-public class YourDbContext : DbContext
+public class YourDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
     public YourDbContext(DbContextOptions<YourDbContext> options) : base(options)
     {
     }
-    public DbSet<Employee> Employees { get; set; }
+    public Microsoft.EntityFrameworkCore.DbSet<Employee> Employees { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<TestTable> TestTable { get; set; }
